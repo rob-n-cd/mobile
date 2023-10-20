19d578e3be581c3289c6eae8161bd50891aefd26
rubymol <ettayi@coffee.com>
2023-10-20 14:49:07 +0530

    auhwe

---
+++ db/oder.sql
@@ -0,0 +1,46 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Oct 20, 2023 at 09:17 AM
+-- Server version: 8.0.31
+-- PHP Version: 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `robin`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oder`
+--
+
+DROP TABLE IF EXISTS `oder`;
+CREATE TABLE IF NOT EXISTS `oder` (
+  `oid` int NOT NULL AUTO_INCREMENT,
+  `oname` varchar(30) NOT NULL,
+  `oaddress` varchar(30) NOT NULL,
+  `pincode` varchar(20) NOT NULL,
+  `place` varchar(30) NOT NULL,
+  `contry` varchar(25) NOT NULL,
+  `proname` varchar(30) NOT NULL,
+  `proprice` int NOT NULL,
+  PRIMARY KEY (`oid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
